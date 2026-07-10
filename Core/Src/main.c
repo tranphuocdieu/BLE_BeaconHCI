2c50f74e484b5f0040ffe49573907b7f4e3346a1
dtran <dieu.tran@datalogic.com>
2026-07-10 15:10:03 +0700

    feat: test timer

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -128,7 +128,12 @@ int main(void)
   {
 
     /* USER CODE END WHILE */
-    MX_APPE_Process();
+    // MX_APPE_Process();
+
+    uint32_t current_time = SYS_GetCurrentTimeMs();
+    HAL_Delay(1000);
+    uint32_t new_time = SYS_GetCurrentTimeMs();
+    HAL_Delay(1000);
 
     /* USER CODE BEGIN 3 */
   }
